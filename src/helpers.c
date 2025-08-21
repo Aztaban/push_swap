82931945a807e8c5c4c5ed72d194f97b5599c3fa
Aztaban <Aztabanee@gmail.com>
2025-08-22 01:55:56 +0200

    added main and helpers for testing instructions

---
+++ src/helpers.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   helpers.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjusta <mjusta@student.42prague.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/22 01:07:42 by mjusta            #+#    #+#             */
+/*   Updated: 2025/08/22 01:43:45 by mjusta           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_stack	*create_stack(void)
+{
+	t_stack *s = malloc(sizeof(t_stack));
+	if (!s)
+		return (NULL);
+	s->head = NULL;
+	s->tail = NULL;
+	s->size = 0;
+	return (s);
+}
+
+void	push_front(t_stack *s, int value)
+{
+	t_node *new = malloc(sizeof(t_node));
+	if (!new)
+		return ;
+	new->value = value;
+	new->next = s->head;
+	s->head = new;
+	if (s->size == 0)
+		s->tail = new;
+	s->size++;
+}
+
+void print_stack(const char *name, t_stack *s)
+{
+	ft_printf("%s: ", name);
+	t_node *tmp = s->head;
+	while (tmp)
+	{
+		ft_printf("%d ", tmp->value);
+		tmp = tmp->next;
+	}
+	ft_printf("\n");
+}
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mjusta <mjusta@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 23:56:16 by mjusta            #+#    #+#             */
-/*   Updated: 2025/08/21 23:57:25 by mjusta           ###   ########.fr       */
+/*   Updated: 2025/08/22 01:54:08 by mjusta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,20 @@
 
 int	main(void)
 {
-	
+	t_stack *a = create_stack();
+	t_stack *b = create_stack();
+
+	push_front(a, 5);
+	push_front(a, 3);
+	push_front(a, 1);
+
+	print_stack("a", a);
+	print_stack("b", b);
+
+	ss(a, b);
+
+	print_stack("a", a);
+	print_stack("b", b);
+
+	return (0);
 }
