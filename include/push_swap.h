bf2fb66cc0df0bc00c90da2757c82e7f4854c967
Aztaban <Aztabanee@gmail.com>
2025-08-22 01:55:22 +0200

    modified header for instructions

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,11 +6,53 @@
 /*   By: mjusta <mjusta@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 23:21:59 by mjusta            #+#    #+#             */
-/*   Updated: 2025/08/21 23:22:32 by mjusta           ###   ########.fr       */
+/*   Updated: 2025/08/22 01:44:07 by mjusta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP
 # define PUSH_SWAP
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdbool.h>
+# include "libft.h"
+
+typedef struct s_node
+{
+	int				value;
+	struct s_node	*next;
+}	t_node;
+
+typedef struct s_stack
+{
+	t_node	*head;
+	t_node	*tail;
+	int		size;
+}	t_stack;
+
+// swap
+void	sa(t_stack *a);
+void	sb(t_stack *b);
+void	ss(t_stack *a, t_stack *b);
+
+// push
+void	pa(t_stack *a, t_stack *b);
+void	pb(t_stack *a, t_stack *b);
+
+// rotate
+void	ra(t_stack *a);
+void	rb(t_stack *b);
+void	rr(t_stack *a, t_stack *b);
+
+// reverse rotate
+void	rra(t_stack *a);
+void	rrb(t_stack *b);
+void	rrr(t_stack *a, t_stack *b);
+
+// helpers
+t_stack	*create_stack(void);
+void	push_front(t_stack *s, int value);
+void	print_stack(const char *name, t_stack *s);
 
 #endif
