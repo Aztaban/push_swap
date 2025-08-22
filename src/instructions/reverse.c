33007d8e009ea1cc555ba1abd919c7e707e2bd58
Martin Justa <mjusta@c1r9s2.42prague.com>
2025-08-22 19:57:01 +0200

    reverse instraction looping

--- src/instructions/reverse.c
+++ src/instructions/reverse.c
@@ -6,14 +6,43 @@
 /*   By: mjusta <mjusta@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 01:01:01 by mjusta            #+#    #+#             */
-/*   Updated: 2025/08/22 01:01:25 by mjusta           ###   ########.fr       */
+/*   Updated: 2025/08/22 19:56:39 by mjusta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static bool	reverse_rotate(t_stack *s);
+static bool	reverse_rotate(t_stack *s)
+{
+	t_node	*new_head;
+	t_node	*new_tail;
 
-void	rra(t_stack *a);
-void	rrb(t_stack *b);
-void	rrr(t_stack *a, t_stack *b);
+	if (s->size < 2)
+		return (false);
+	new_head = s->tail;
+	new_tail = s->head->next;
+	while (new_tail->next && new_tail->next != s->tail)
+		new_tail = new_tail->next;
+	new_head->next = s->head;
+	s->head = new_head;
+	s->tail = new_tail;
+	return (true);
+}
+
+void	rra(t_stack *a)
+{
+	if (reverse_rotate(a))
+		ft_printf("rra\n");
+}
+
+void	rrb(t_stack *b)
+{
+	if (reverse_rotate(b))
+		ft_printf("rrb\n");	
+}
+
+void	rrr(t_stack *a, t_stack *b)
+{
+	if (reverse_rotate(a) | reverse_rotate(b))
+		ft_printf("rrr\n");
+}
