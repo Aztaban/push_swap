4a24f4f8acad2866c3d13a55a53a1d592899789a
Aztaban <Aztabanee@gmail.com>
2025-08-29 01:08:57 +0200

    added comments

--- src/helpers.c
+++ src/helpers.c
@@ -6,12 +6,15 @@
 /*   By: mjusta <mjusta@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 01:07:42 by mjusta            #+#    #+#             */
-/*   Updated: 2025/08/25 14:48:42 by mjusta           ###   ########.fr       */
+/*   Updated: 2025/08/29 01:06:34 by mjusta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+/**
+ * @brief Allocates and initializes a new empty stack.
+ */
 t_stack	*create_stack(void)
 {
 	t_stack	*s;
@@ -25,6 +28,9 @@ t_stack	*create_stack(void)
 	return (s);
 }
 
+/**
+ * @brief Pushes a value to the front (head) of the stack.
+ */
 void	push_front(t_stack *s, int value)
 {
 	t_node	*new;
@@ -40,6 +46,9 @@ void	push_front(t_stack *s, int value)
 	s->size++;
 }
 
+/**
+ * @brief Pushes a value to the back (tail) of the stack.
+ */
 void	push_back(t_stack *s, int value)
 {
 	t_node	*new;
@@ -57,20 +66,9 @@ void	push_back(t_stack *s, int value)
 	s->size++;
 }
 
-void	print_stack(const char *name, t_stack *s)
-{
-	t_node	*tmp;
-
-	ft_printf("%s: ", name);
-	tmp = s->head;
-	while (tmp)
-	{
-		ft_printf("%d ", tmp->value);
-		tmp = tmp->next;
-	}
-	ft_printf("\n");
-}
-
+/**
+ * @brief Frees all nodes in the stack and the stack struct itself.
+ */
 void	free_stack(t_stack *s)
 {
 	t_node	*current;
@@ -84,4 +82,21 @@ void	free_stack(t_stack *s)
 		current = next;
 	}
 	free(s);
+}
+
+/**
+ * @brief Prints all values in the stack from head to tail.
+ */
+void	print_stack(const char *name, t_stack *s)
+{
+	t_node	*tmp;
+
+	ft_printf("%s: ", name);
+	tmp = s->head;
+	while (tmp)
+	{
+		ft_printf("%d ", tmp->value);
+		tmp = tmp->next;
+	}
+	ft_printf("\n");
 }
